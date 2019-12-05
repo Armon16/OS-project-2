308598f3d629ffe14e95d1c3f73b64e3ff26bbdf
armon16 <armon16@csu.fullerton.edu>
2019-12-05 10:10:52 -0800

    started initing variables

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+const int MAX_RUNTIME = 100000;
+const int MAX_MEMORY_SIZE = 30000;
+
+int main(){
+
+}
